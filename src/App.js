43e9ce6a7ed42ff1ed409415ffc8f730290c9658
Dario Cruz <dcruz@altimetrik.com>
2025-06-24 14:46:19 -0300

    Changing colors & desktop layout

--- src/App.js
+++ src/App.js
@@ -52,102 +52,100 @@ function App() {
           <p></p>
         </div>
       </div>
-      <div className="resume-doc__content content-experience">
-        <h2>Experience</h2>
-        {resume.experience.map(
-          ({
-            company,
-            title,
-            start_date,
-            end_date,
-            description,
-            responsibilities,
-          }) => (
-            <div
-              className="content-experience--job"
-              key={`${company}-${title}-${start_date}-${end_date}`}
-            >
-              <h3>
-                {company}: {title}
-              </h3>
-              <h4>
-                {start_date} - {end_date}
-              </h4>
-              <p>{description}</p>
-              <ul>
-                {responsibilities.map((responsibility) => (
-                  <li key={responsibility}>{responsibility}</li>
-                ))}
-              </ul>
-            </div>
-          )
-        )}
-      </div>
-
-      <div className="resume-doc__content content-experience">
-        <h2>Education</h2>
-        {resume.education.map(
-          ({ degree, institution, status, description }) => (
-            <div
-              className="content-experience--job"
-              key={`${degree}-${institution}-${status}`}
-            >
-              <h3>
-                {degree} - {institution}
-              </h3>
-              <h4>{status}</h4>
-              <p>{description}</p>
-            </div>
-          )
-        )}
-      </div>
-
-      <div className="resume-doc__content content-experience">
-        <h2>Main Skills</h2>
-        <div className="content-experience--job">
-          {resume.skills.join(" - ")}
+      <div className="resume-doc__content">
+        {" "}
+        <div className="content-experience">
+          <h2>Experience</h2>
+          {resume.experience.map(
+            ({
+              company,
+              title,
+              start_date,
+              end_date,
+              description,
+              responsibilities,
+            }) => (
+              <div
+                className="content-experience--job"
+                key={`${company}-${title}-${start_date}-${end_date}`}
+              >
+                <h3>
+                  {company}: {title}
+                </h3>
+                <h4>
+                  {start_date} - {end_date}
+                </h4>
+                <p>{description}</p>
+                <ul>
+                  {responsibilities.map((responsibility) => (
+                    <li key={responsibility}>{responsibility}</li>
+                  ))}
+                </ul>
+              </div>
+            )
+          )}
         </div>
-      </div>
-
-      <div className="resume-doc__content content-experience">
-        <h2>Side Projects</h2>
-        {resume.side_projects.map(({ name, url }) => (
-          <div className="content-experience--job" key={`${name}`}>
-            <h3>{name}</h3>
-            <div className="url-wrapper">
-              <a href={url}>{url}</a>
-            </div>
+        <div className="content-experience">
+          <h2>Education</h2>
+          {resume.education.map(
+            ({ degree, institution, status, description }) => (
+              <div
+                className="content-experience--job"
+                key={`${degree}-${institution}-${status}`}
+              >
+                <h3>
+                  {degree} - {institution}
+                </h3>
+                <h4>{status}</h4>
+                <p>{description}</p>
+              </div>
+            )
+          )}
+        </div>
+        <div className="content-experience">
+          <h2>Main Skills</h2>
+          <div className="content-experience--job">
+            {resume.skills.join(" - ")}
           </div>
-        ))}
-      </div>
-
-      <div className="resume-doc__content content-experience">
-        <h2>Articles</h2>
-        {resume.articles.map(({ title, url }) => (
-          <div className="content-experience--job" key={`${title}`}>
-            <div className="url-wrapper">
-              <a href={url}>{title}</a>
-            </div>
-          </div>
-        ))}
-      </div>
-
-      <div className="resume-doc__content content-experience">
-        <h2>Certifications</h2>
-        {resume.certifications.map(({ name, issuer, date, url, details }) => (
-          <div className="content-experience--job" key={`${name}-${issuer}`}>
-            <h3>
-              {name} {issuer ? " - " + issuer : ""}
-            </h3>
-            {date && <h4>{date}</h4>}
-            {url && (
+        </div>
+        <div className="content-experience">
+          <h2>Side Projects</h2>
+          {resume.side_projects.map(({ name, url }) => (
+            <div className="content-experience--job" key={`${name}`}>
+              <h3>{name}</h3>
               <div className="url-wrapper">
                 <a href={url}>{url}</a>
               </div>
-            )}
-            <p>{details}</p>
-          </div>
-        ))}
+            </div>
+          ))}
+        </div>
+        <div className="content-experience">
+          <h2>Articles</h2>
+          {resume.articles.map(({ title, url }) => (
+            <div className="content-experience--job" key={`${title}`}>
+              <div className="url-wrapper">
+                <a href={url}>{title}</a>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="content-experience">
+          <h2>Certifications</h2>
+          {resume.certifications.map(({ name, issuer, date, url, details }) => (
+            <div className="content-experience--job" key={`${name}-${issuer}`}>
+              <h3>
+                {name} {issuer ? " - " + issuer : ""}
+              </h3>
+              {date && <h4>{date}</h4>}
+              {url && (
+                <div className="url-wrapper">
+                  <a href={url}>{url}</a>
+                </div>
+              )}
+              <p>{details}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
